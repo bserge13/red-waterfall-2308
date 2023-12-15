8df5e08b1342ed6057311849a8e89034eb3af855
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-15 11:56:18 -0600

    Facade spec.

--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SearchFacade do 
-  
+  describe '#members_by_nation', :vcr do 
+    it 'returns an array of Member objects' do 
+      members = SearchFacade.members_by_nation("Fire Nation")
+
+      expect(members).to be_an Array 
+      expect(members.first).to be_a Member 
+      expect(members.first.name).to be_a String
+      expect(members.first.enemies).to be_an Array
+      expect(members.first.allies).to be_an Array
+    end
+  end
 end
