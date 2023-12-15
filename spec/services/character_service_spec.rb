9f87a158c6e250467a863ca98ab6843a8218746d
Blake Sergesketter <blakesergesketter@gmail.com>
2023-12-15 11:59:30 -0600

    Incomplete service spec testing.

--- spec/services/character_service_spec.rb
+++ spec/services/character_service_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CharacterService do 
-  
+  describe 'class method', :vcr do
+    it '#nation_members' do
+      response = CharacterService.nation_members("Fire Nation")
+
+      expect(response).to be_an Array  
+      
+    end 
+  end
 end
